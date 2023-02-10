4cb6b8128937b71c0927c78c153ee9335367c711
Yago Ferreira <yagojj@gmail.com>
2023-02-10 09:40:38 -0300

    feat: added service layer

---
+++ inventory/src/services/product/product_service.py
@@ -0,0 +1,20 @@
+from typing import List
+from src.repositories.product.product_repository import ProductRepository
+
+
+class ProductService:
+    @staticmethod
+    def get_all_products():
+        response = ProductRepository.get_all_products()
+
+        print(response)
+
+        return response
+
+    @staticmethod
+    def get_product_by_id(pk: str):
+        return ProductRepository.get_product_by_id(pk)
+
+    @staticmethod
+    def delete_product_by_id(pk: str):
+        return ProductRepository.delete_product_by_id(pk)
