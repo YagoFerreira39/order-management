4d0ad38b21c9104f3bbabe0a7f420af280e5034e
Yago Ferreira <yagojj@gmail.com>
2023-02-20 02:06:28 -0300

    fix: order routers

--- payment/src/routers/order/order_routers.py
+++ payment/src/routers/order/order_routers.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter
+
+from src.controllers.order.order_controller import OrderController
+from starlette.requests import Request
 
 
 class OrderRouters:
@@ -11,4 +14,19 @@ class OrderRouters:
     @staticmethod
     @__order_router.get("/get_all_orders", tags=["Order"])
     def get_all_orders():
-        return []
+        response = OrderController.get_all_orders()
+        return response
+
+    @staticmethod
+    @__order_router.post("/create_order", tags=["Order"])
+    async def create_order(request: Request):
+        body = await request.json()
+
+        order_input = {
+            "product_id": body["product_id"],
+            "customer_id": body["customer_id"],
+            "quantity": body["quantity"]
+        }
+        print(order_input)
+        response = OrderController.create_order(order_input)
+        return response
