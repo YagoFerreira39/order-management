7410fe0f0d64eb8acbc4717cae2e6de49d1c2ac5
Yago Ferreira <yagojj@gmail.com>
2023-03-01 19:35:57 -0300

    fix: order model

--- orders/src/domain/models/order_model.py
+++ orders/src/domain/models/order_model.py
@@ -1,9 +1,9 @@
-from typing import TypedDict
+from pydantic import BaseModel
 
 from src.domain.enums.order.order_status_enum import OrderStatusEnum
 
 
-class OrderModel(TypedDict):
+class OrderModel(BaseModel):
     unique_id: str
     product_id: str
     account_id: str
@@ -11,7 +11,7 @@ class OrderModel(TypedDict):
     fee: float
     total_price: float
     quantity: int
-    status: OrderStatusEnum  # pending, completed, refunded
+    status: str  # pending, completed, refunded
     type: str
     market: str
     region: str
