f339c0bad88c2a78b7edf7f5ac03f20a553816c2
Yago Ferreira <yagojj@gmail.com>
2023-03-10 09:03:58 -0300

    wip: order producer

--- orders/src/infrastructure/kafka/producers/order_producer.py
+++ orders/src/infrastructure/kafka/producers/order_producer.py
@@ -1,10 +1,20 @@
 import json
 
+from decouple import config
 from kafka import KafkaProducer
 
 
 class OrderProducer:
-    @staticmethod
-    def send_order(topic: str, data):
-        order_producer = KafkaProducer(bootstrap_servers="localhost:29092")
-        return order_producer.send(topic, json.dumps(data).encode("utf-8"))
+    __producer = None
+
+    @classmethod
+    def get_producer(cls) -> KafkaProducer:
+        if cls.__producer is None:
+            producer_config = {
+                "bootstrap_servers": "localhost:29092",
+                "client_id": config("KAFKA_CLIENT_ID")
+            }
+
+            cls.__producer = KafkaProducer(**producer_config)
+
+        return cls.__producer
