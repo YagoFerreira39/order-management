116c31e7f52559c52efec89f661aedd55bc3c119
Yago Ferreira <yagojj@gmail.com>
2023-03-12 15:44:46 -0300

    fix: changed order controller and service

--- orders/src/controllers/order/order_controller.py
+++ orders/src/controllers/order/order_controller.py
@@ -3,30 +3,28 @@ from src.services.order.order_service import OrderService
 
 
 class OrderController:
-    __order_service = OrderService
-
-    @classmethod
-    def get_all_orders(cls):
-        result = cls.__order_service.get_all_orders()
+    @staticmethod
+    def get_all_orders():
+        result = OrderService.get_all_orders()
         return result
 
-    @classmethod
-    def get_order_by_id(cls, order_id: str):
-        result = cls.__order_service.get_order_by_id(order_id)
+    @staticmethod
+    def get_order_by_id(order_id: str):
+        result = OrderService.get_order_by_id(order_id)
         return result
 
-    @classmethod
-    async def create_order(cls, order_input):
-        result = await cls.__order_service.create_order(order=order_input)
+    @staticmethod
+    async def create_order(order_input):
+        result = await OrderService.create_order(order=order_input)
         return result
 
-    @classmethod
-    async def update_order(cls, order_id, order_updated_data):
-        result = await cls.__order_service.update_order_by_id(
+    @staticmethod
+    async def update_order(order_id, order_updated_data):
+        result = await OrderService.update_order_by_id(
             order_id, order_updated_data
         )
         return result
 
-    @classmethod
-    def delete_order_by_id(cls, order_id: str):
-        result = cls.__order_service.delete_order_by_id(order_id)
+    @staticmethod
+    def delete_order_by_id(order_id: str):
+        result = OrderService.delete_order_by_id(order_id)
--- orders/src/services/order/order_service.py
+++ orders/src/services/order/order_service.py
@@ -1,56 +1,53 @@
 import json
+import requests
 
+from typing import NoReturn
+from decouple import config
+
+from src.domain.dtos.order.order_dto import OrderDTO
 from src.domain.extensions.order.order_extensions import OrderExtension
 from src.domain.types.order_input import OrderInput
 from src.infrastructure.kafka.producers.order_producer import OrderProducer
 from src.repositories.order.order_repository import OrderRepository
 
-from decouple import config
-
-import requests
-
 
 class OrderService:
     __order_repository = OrderRepository
 
-    @classmethod
-    def get_all_orders(cls):
-        response = cls.__order_repository.get_all_orders()
+    @staticmethod
+    def get_all_orders():
+        response = OrderRepository.get_all_orders()
         return response
 
-    @classmethod
-    def get_order_by_id(cls, order_id: str):
-        response = cls.__order_repository.get_order_by_id(order_id)
+    @staticmethod
+    def get_order_by_id(order_id: str):
+        response = OrderRepository.get_order_by_id(order_id)
 
         return response
 
-    @classmethod
-    async def create_order(cls, order: OrderInput):
-        topic = config("NEW_ORDER_TOPIC_NAME")
-
-        product_data = cls.__get_product_in_order(order["product_id"])
+    @staticmethod
+    async def create_order(order: OrderInput):
+        product_data = OrderService.__get_product_in_order(order["product_id"])
         formatted_order_model = OrderExtension.to_model(order, product_data)
 
-        response = await cls.__order_repository.create_order(formatted_order_model)
+        response = await OrderRepository.create_order(formatted_order_model)
 
         order_dto = OrderExtension.to_dto(response["result"])
 
-        order_producer = OrderProducer.get_producer()
-        order_producer.send(topic=topic, value=json.dumps(order_dto).encode("utf-8"))
-        order_producer.flush()
+        OrderService.__send_order_to_producer(order_dto)
 
         return response
 
-    @classmethod
-    async def update_order_by_id(cls, order_id, order_updated_data):
-        response = await cls.__order_repository.update_order_by_id(
+    @staticmethod
+    async def update_order_by_id(order_id: str, order_updated_data):
+        response = await OrderRepository.update_order_by_id(
             order_id, order_updated_data
         )
         return response
 
-    @classmethod
-    def delete_order_by_id(cls, order_id: str):
-        response = cls.__order_repository.delete_order_by_id(order_id)
+    @staticmethod
+    def delete_order_by_id(order_id: str):
+        response = OrderRepository.delete_order_by_id(order_id)
         return response
 
     @staticmethod
@@ -60,3 +57,11 @@ class OrderService:
         )
         product_data = product_request.json()
         return product_data
+
+    @staticmethod
+    def __send_order_to_producer(order: OrderDTO) -> NoReturn:
+        topic = config("NEW_ORDER_TOPIC_NAME")
+
+        order_producer = OrderProducer.get_producer()
+        order_producer.send(topic=topic, value=json.dumps(order).encode("utf-8"))
+        order_producer.flush()
