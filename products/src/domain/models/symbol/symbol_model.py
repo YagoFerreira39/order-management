9b810e673672a80f81889a1ef711002d56c87f2c
Yago Ferreira <yagojj@gmail.com>
2023-05-01 08:23:09 -0300

    feat: added symbol model

---
+++ products/src/domain/models/symbol/symbol_model.py
@@ -0,0 +1,11 @@
+class SymbolModel:
+    symbol: str
+    last_refreshed: str
+    open: float
+    high: float
+    low: float
+    adjusted_close: float
+    volume: int
+    dividend_amount: float
+    split_coefficient: float
+    
